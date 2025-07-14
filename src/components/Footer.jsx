95579179e1a3cd56445c15bc6c97fa763ce3afd2
Dev-Rodiyat <olajideadedoyinrodiyat@gmail.com>
2025-07-14 15:06:46 +0100

    Fixed footer content and font size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="text-center text-sm pt-10 border-t dark:border-gray-700">
-            <p>© {new Date().getFullYear()} WordWise. Built with ❤️ using React & Tailwind.</p>
+        <footer className="text-center sm:text-base text-sm py-5 border-t dark:border-gray-700">
+            <p>© {new Date().getFullYear()} WordWise. Built with ❤️ using React, Vite & Tailwind.</p>
         </footer>
     )
 }
